0993ee198f076a591b796e0da0f2b31c5bfae8b0
tuxite <morinmatthieu@gmail.com>
2014-10-12 15:33:02 +0200

    Added a function to toggle the allowances in the settings.

--- settings/static/js/settings/import.js
+++ settings/static/js/settings/import.js
@@ -28,10 +28,45 @@
         });
     }
 
+    /* Function to delete an item when clicking on the delete button */
+    function toggle_item() {
+        $(document).on('click', '.btn-toggle', function (e) {
+            e.preventDefault();
+            // Reset
+            $('.form-feedback').remove();
+
+            var button = $(e.target).closest('a'),
+                item = button.parents('tr:first');
+
+            // Set the loading state
+            button.button('loading');
+            // call the link
+            $.ajax({
+                url: $(button).attr('href'),
+                success: function (data) {
+                    // Reset the button
+                    button.button('reset');
+                    // Update the row
+                    item.replaceWith(data.content);
+                },
+                error: function (data) {
+                    console.log(data.responseText);
+                    // Process the error message
+                    var errors = JSON.parse(data.responseText);
+                    // Reset the button
+                    button.button('reset');
+                    // Return an error message
+                    button.parent().append("<span class='text-danger form-feedback'><b>" + errors.error + "</b></span>");
+                }
+            });
+        });
+    }
+
     /* DOM ready */
     $(function () {
         /* Loading functions */
         delete_item();
+        toggle_item();
     });
 
 }(jQuery));
