e385c0d3f78588b951db76f1d1480f039c5aa600
tuxite <morinmatthieu@gmail.com>
2014-10-12 15:21:44 +0200

    Added a placeholder and forced format for Datefields

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8; -*-
-from django import forms
+from django.forms import widgets
+from django.utils import formats
 
-class DateInput(forms.DateInput):
+class DateInput(widgets.DateTimeBaseInput):
+    """Customized class for Date inputs with the HTML5 date type and
+    placeholder for browsers not supporting this HTML5 type."""
     input_type = 'date'
+
+    def __init__(self, attrs=None, format=None):
+        super(DateInput, self).__init__(attrs)
+        self.format = '%Y-%m-%d'
+        self.attrs = {'placeholder': 'YYYY-MM-DD'}
+
+    def _format_value(self, value):
+        return formats.localize_input(value, self.format)
