f6a8c37cdc33793dac6cf118570fa027166b0d8b
tuxite <morinmatthieu@gmail.com>
2014-10-12 14:54:34 +0200

    Removed header

--- common/js/common.js
+++ common/js/common.js
@@ -1,31 +1,3 @@
-/*
- * Javascript
- *
- * (c) 2013 Association DSM, http://devmaretique.com
- *
- * This file is part of Pharmaship.
- *
- * Pharmaship is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * any later version.
- *
- * Pharmaship is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Pharmaship.  If not, see <http://www.gnu.org/licenses/>.
- *
- * =====================================================================
- * Filename:    common/js/common.js
- * Description: Common functions
- * Author:      Matthieu Morin
- * Version:     0.2
- * =====================================================================
- */
-
 /* jQuery functions */
 (function ($) {
     /* DOM ready */
