67eed9fdae41b0d68c22cbbe7dd8073039ecc200
tuxite <morinmatthieu@gmail.com>
2014-11-23 16:16:03 +0800

    Fix #6: Added an access to the doc from the user button.
    The documentation must be compiled to be accessible.
    In production, this documentation must be serve apart from WSGI.

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8; -*-
 from django.conf.urls import patterns, url
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = patterns('core.views',
     # General
@@ -10,3 +12,7 @@ urlpatterns += patterns('',
     # Contact
     url(r'^contact$', 'inventory.views_common.contact'),
     )
+
+# Access to the documentation, only in DEBUG mode.
+# In production these files must be treated by the server Apache or NGinx.
+urlpatterns += static(settings.DOCS_URL, document_root=settings.DOCS_ROOT)
